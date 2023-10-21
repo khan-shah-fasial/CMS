f7fcb55e25bee2641c2f2e1eeaaea6120a113f7e
Faisal <khanfasial.makent@gmail.com>
2023-10-21 11:53:07 +0530

    test

---
+++ resources/views/backend/partials/meta.blade.php
@@ -0,0 +1,5 @@
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">    
+<title>Dashboard | Hyper - Responsive Bootstrap 5 Admin Dashboard</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
+<meta content="Coderthemes" name="author">
