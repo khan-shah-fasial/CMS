5b8af1ea4cca377e856e3c788d5cd56a865689ee
Rashid Ansari <rashid.makent@gmail.com>
2023-10-25 15:44:01 +0530

    added service db migration and model

---
+++ app/Models/PracticeArea.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PracticeArea extends Model
+{
+    use HasFactory;
+    
+    protected $table = 'practice_areas';
+
+    protected $fillable = [
+        'parent_id',
+        'thumnail_image',
+        'title',
+        'content',
+        'why_choose_us',
+        'faq',
+        'meta_title',
+        'meta_description',
+        'breadcrumb_title',
+        'breadcrumb_subtitle',
+        'breadcrumb_image',
+        'status',
+    ];    
+}
