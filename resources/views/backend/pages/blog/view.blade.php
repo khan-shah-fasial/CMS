ea9de9d854cabd1191f03ed9f5e9f9f65422b535
Rashid Ansari <rashid.makent@gmail.com>
2023-10-27 18:58:19 +0530

    changes

--- resources/views/backend/pages/blog/view.blade.php
+++ resources/views/backend/pages/blog/view.blade.php
@@ -3,5 +3,5 @@
     <p><strong>Short Description:</strong> {{$blog->short_description}}</p>
     <p><strong>Slug:</strong> {{$blog->slug}}</p>
     <p><strong>Image:</strong></p> <img src="{{ asset('storage/' . $blog->main_image) }}" class="img-thumbnail">
-    <p><strong>Content:</strong> {{$blog->content}}</p>
+    <p><strong>Content:</strong>@php echo html_entity_decode(html_entity_decode($blog->content)) @endphp</p>
 </div>
