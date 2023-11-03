eada64aa25169b1a110e83a6fc81b6370a8acd9d
Rashid Ansari <rashid.makent@gmail.com>
2023-11-03 11:52:59 +0530

    changes in blog manage

--- resources/views/backend/pages/blog/view.blade.php
+++ resources/views/backend/pages/blog/view.blade.php
@@ -5,12 +5,28 @@
    </div>
    
    <div class="col-md-6">
-       <p><strong>Slug:</strong> {{$blog->slug}}</p>
+       <p><strong>Slug:</strong> 
+       <a target="_blank" href="{{ url(route('blog.detail', ['category' => DB::table('blog_categories')->where('id', json_decode($blog->blog_category_ids)[0])->first()->slug, 'slug' => $blog->slug] )) }}">
+   {{$blog->slug}}
+</a> 
+     </p>
    </div>
    
    <div class="col-md-12">
         <p><strong>Short Description:</strong> {{$blog->short_description}}</p>
    </div>
+
+   <div class="col-md-12">
+    <p><strong>Categories:</strong>
+     @foreach(json_decode($blog->blog_category_ids) as $key => $categoryId)
+        <?php $category = DB::table('blog_categories')->where('id', $categoryId)->first(); ?>
+        {{ $category->name }}
+        @if ($key < count(json_decode($blog->blog_category_ids)) - 1)
+            ,
+        @endif
+     @endforeach
+    </p>
+</div>  
    
    <div class="col-md-12">
         <p><strong>Image:</strong></p> <img src="{{ asset('storage/' . $blog->main_image) }}" class="img-thumbnail">
