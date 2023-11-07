083273131ca40d08fce8c5f5a9dd7e10723046e4
Mahtab Alam <mehtab.makent@gmail.com>
2023-11-07 18:43:33 +0530

    frontend mahtab

--- resources/views/frontend/pages/blog/detail.blade.php
+++ resources/views/frontend/pages/blog/detail.blade.php
@@ -20,8 +20,10 @@
 
 <!-- -------------------- blog details banner start ---------------- -->
 
-<section class="blog_details_page_banner" style="background-image: url({{ asset('storage/' . $detail->main_image) }});">
-</section>
+<div class="blog_details_page_banner">
+<img src="{{ asset('storage/' . $detail->main_image) }}">
+
+</div>
 
 <!-- -------------------- blog details banner end   ---------------- -->
 
