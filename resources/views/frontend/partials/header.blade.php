511f8de1856b8a85edab69c823d3685cc33cc82d
Faisal <khanfasial.makent@gmail.com>
2023-11-14 15:39:22 +0530

    data add

--- resources/views/frontend/partials/header.blade.php
+++ resources/views/frontend/partials/header.blade.php
@@ -1,5 +1,9 @@
 @php
-$practice_Area = DB::table('practice_areas')->where('parent_id', null)->limit(8)->orderBy('series', 'asc')->get();
+$practice_Area = DB::table('practice_areas')
+    ->whereIn('id', ['7', '9', '11', '8', '10', '12', '13', '14'])
+    ->orderByRaw("FIELD(id, '7', '9', '11', '8', '10', '12', '13', '14')")
+    ->get();
+
 @endphp
 
 <!--------------Header start----------------->
