d7bea74e75b540c55a9e699e826c4339773ef729
Mahtab Alam <mehtab.makent@gmail.com>
2023-12-13 11:04:30 +0530

    blog page changes

--- resources/views/frontend/component/blog.blade.php
+++ resources/views/frontend/component/blog.blade.php
@@ -87,6 +87,30 @@ $right_news = DB::table('blogs')->where('status', 1)->whereJsonContains('blog_ca
                         class="main_heading_color text-decoration-none cursor-pointer">Newscast!</a>
                 </h2>
 
+
+    <div class="marquee main_marque ">
+            @foreach ($right_news as $row)
+
+                <div class="item">
+                  <div class="blog_right_box" data-aos-once="true" data-aos="fade-up">
+                    <img src="{{ asset('storage/' .$row->main_image) }}" alt="" />
+                    <div>
+                        <p class="desc">
+                            {{ $row->title }}
+                        </p>
+                        <a
+                            href="{{ url(route('blog.detail', ['category' =>'news', 'slug' => strtolower(str_replace(' ', '-',$row->slug))] )) }}">View
+                            More</a>
+                    </div>
+                  </div>
+                </div>
+               
+            @endforeach
+      </div>
+
+                
+
+                <!-- <marquee direction="up">
                 @foreach ($right_news as $row)
                 <div class="item">
                   <div class="blog_right_box" data-aos-once="true" data-aos="fade-up">
@@ -103,14 +127,12 @@ $right_news = DB::table('blogs')->where('status', 1)->whereJsonContains('blog_ca
                 </div>
                
                 @endforeach
+                </marquee> -->
 
             </div>
         </div>
     </div>
 </section>
-
-	
-	
 @endif
 
 <!----------============== blog =============------------->
