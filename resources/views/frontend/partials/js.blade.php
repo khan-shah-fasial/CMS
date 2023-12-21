c6462dc7712620f3601b72d175fc7abfe593d633
Mahtab Alam <mehtab.makent@gmail.com>
2023-12-21 16:27:27 +0530

    chnages

--- resources/views/frontend/partials/js.blade.php
+++ resources/views/frontend/partials/js.blade.php
@@ -15,8 +15,8 @@
 <!--owl carousel js-->
 <script  src="/assets/frontend/js/owl.carousel.js"></script>
 
-<!--google translate js
-<script defer src="/assets/frontend/js/google_translate.js"></script>-->
+<!--google translate js-->
+<script defer src="/assets/frontend/js/google_translate.js"></script>
 
 <!--moment js-->
 <script src="/assets/frontend/js/moment.min.js"></script>
@@ -36,8 +36,8 @@
 <!--whatsapp js-->
 <script defer src="/assets/frontend/js/whatsapp-chat-support.js"></script>
 
-<!--Google capcha js
-<script src="https://www.google.com/recaptcha/api.js" defer></script>-->
+<!--Google capcha js-->
+<script src="https://www.google.com/recaptcha/api.js" defer></script>
 <!--<script src="/assets/frontend/js/api.js"></script>-->
 
 <!--Custom Js-->
