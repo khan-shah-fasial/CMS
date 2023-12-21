1e96bae663aa82725d4bdb68d5afbef46b9ab372
Mahtab Alam <mehtab.makent@gmail.com>
2023-12-21 18:15:03 +0530

    comment meta tag

--- resources/views/frontend/layouts/app.blade.php
+++ resources/views/frontend/layouts/app.blade.php
@@ -16,10 +16,10 @@
 <body>
 
     
-<!-- Google Tag Manager (noscript) -->
+<!-- Google Tag Manager (noscript) 
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T93DNMW"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+ -->
 
     <!----=========topbar=========----->
 
