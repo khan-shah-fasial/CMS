0e8104bbc83d7d217f466b05eab5a844ccbe9f85
Mahtab Alam <mehtab.makent@gmail.com>
2023-12-21 16:11:00 +0530

    js file chnages

--- resources/views/frontend/partials/js.blade.php
+++ resources/views/frontend/partials/js.blade.php
@@ -1,6 +1,6 @@
-<!-- <script src="/assets/frontend/js/jquery.min.js"></script>
-<script src="/assets/frontend/js/bootstrap.bundle.min.js"></script> -->
-<script src="/assets/frontend/js/combine.min.js"></script>
+<script src="/assets/frontend/js/jquery.min.js"></script>
+<script src="/assets/frontend/js/bootstrap.bundle.min.js"></script> 
+<!--<script src="/assets/frontend/js/combine.min.js"></script>-->
 
 <!--aos js-->
 <script src="/assets/frontend/js/aos.js"></script>
@@ -15,8 +15,8 @@
 <!--owl carousel js-->
 <script  src="/assets/frontend/js/owl.carousel.js"></script>
 
-<!--google translate js-->
-<script defer src="/assets/frontend/js/google_translate.js"></script>
+<!--google translate js
+<script defer src="/assets/frontend/js/google_translate.js"></script>-->
 
 <!--moment js-->
 <script src="/assets/frontend/js/moment.min.js"></script>
