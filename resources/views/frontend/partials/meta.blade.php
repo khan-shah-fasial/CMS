aac4c16ed75c307ff4798f0aafe441bd223c6804
Mahtab Alam <mehtab.makent@gmail.com>
2023-12-29 10:46:54 +0530

    changes all

--- resources/views/frontend/partials/meta.blade.php
+++ resources/views/frontend/partials/meta.blade.php
@@ -126,7 +126,7 @@ $url = url()->current();
 
 <!---------------- Contact Address Schema end ------------------->
 
-<!-- Yandex.Metrika counter
+<!-- Yandex.Metrika counter-->
 <script type="text/javascript" >
   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
   m[i].l=1*new Date();
@@ -143,15 +143,21 @@ $url = url()->current();
 </script>
 
 <noscript><div><img src="https://mc.yandex.ru/watch/95221741" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
--->
 
 
-<!-- Google Tag Manager -->
+<!-- Google tag (gtag.js)  -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0N029ZM0WG"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-0N029ZM0WG'); </script>
+
+
+<!-- Google tag (gtag.js)  -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GTLLQV0KXE"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-GTLLQV0KXE'); </script>
+
+<!-- Google Tag Manager  -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
   })(window,document,'script','dataLayer','GTM-T93DNMW');</script>
- 
+
 
 <base id="baseUrl" href="{{url('')}}">
