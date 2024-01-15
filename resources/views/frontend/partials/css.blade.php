e87fc98c9af4bc045130b90f9aa58f344947dd23
Mahtab Alam <mehtab.makent@gmail.com>
2024-01-15 16:19:19 +0530

    css put file

--- resources/views/frontend/partials/css.blade.php
+++ resources/views/frontend/partials/css.blade.php
@@ -1,6 +1,6 @@
 <link rel="stylesheet" href="/assets/frontend/css/bootstrap.min.css" />
 <link rel="stylesheet" href="/assets/frontend/css/style.css" />
-<link rel="stylesheet" href="/assets/frontend/css/responsive.css" />
+<!-- <link rel="stylesheet" href="/assets/frontend/css/responsive.css" /> -->
 <!-- <link rel="stylesheet" href="/assets/frontend/css/aos.css" /> -->
 
 <!-- select2 css -->
@@ -12,5 +12,5 @@
 <link rel="preload" href="/assets/frontend/css/toastr.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
    <!-- Preload Font Awesome CSS -->
    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" as="style" onload="this.rel='stylesheet'">
-<link rel="stylesheet" href="/assets/frontend/css/intlTelInput.css">
+<!-- <link rel="stylesheet" href="/assets/frontend/css/intlTelInput.css"> -->
 
