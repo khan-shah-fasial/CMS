c1c8d05f7e77022f033cd4df1d590b853bb5d69c
Mahtab Alam <mehtab.makent@gmail.com>
2024-01-15 15:59:01 +0530

    put css file in style css file

--- resources/views/frontend/partials/css.blade.php
+++ resources/views/frontend/partials/css.blade.php
@@ -1,13 +1,14 @@
 <link rel="stylesheet" href="/assets/frontend/css/bootstrap.min.css" />
 <link rel="stylesheet" href="/assets/frontend/css/style.css" />
 <link rel="stylesheet" href="/assets/frontend/css/responsive.css" />
-<link rel="stylesheet" href="/assets/frontend/css/aos.css" />
+<!-- <link rel="stylesheet" href="/assets/frontend/css/aos.css" /> -->
 
 <!-- select2 css -->
 <link rel="preload" href="/assets/frontend/css/select2.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
 
-<link rel="stylesheet" href="/assets/frontend/css/owl.carousel.min.css" />
+<!-- <link rel="stylesheet" href="/assets/frontend/css/owl.carousel.min.css" /> -->
 <link rel="stylesheet" href="/assets/frontend/css/owl.theme.default.min.css" />
+<!-- <link rel="stylesheet" href="/assets/frontend/css/whatsapp-chat-support.css" /> -->
 <link rel="preload" href="/assets/frontend/css/toastr.min.css" as="style" onload="this.onload=null;this.rel='stylesheet'">
    <!-- Preload Font Awesome CSS -->
    <link rel="preload" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" as="style" onload="this.rel='stylesheet'">
