3c631f760c281c87d1b205155da5948efb4ab0d9
Faisal <khanfasial.makent@gmail.com>
2024-01-19 17:11:43 +0530

    get in touch updated by fasial

--- resources/views/frontend/pages/privacypolice/index.blade.php
+++ resources/views/frontend/pages/privacypolice/index.blade.php
@@ -49,27 +49,7 @@
 
     <!-- ---------- service get in touch  start ---------------------- -->
 
-    <section class="service_get_in_touch">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="text-center">
-                        <h1 class="main_heading" data-aos-once="true" data-aos="fade-up">
-                            GET IN TOUCH WITH US TODAY
-                        </h1>
-                        <p class="desc" data-aos-once="true" data-aos="fade-up">
-                            We can be reached at
-                        </p>
-                        <a href="#" class="d-flex align-items-center justify-content-center gap-2" data-aos-once="true"
-                            data-aos="fade-up">
-                            <span><img src="/assets/frontend/images/phone.png" alt="" /></span>
-                            <span>011-41023400</span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    @include('frontend.component.get_in_touch')
 
     <!-- -------------- service get in touch  end ---------------------- -->
 
